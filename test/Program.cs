97e660847270a2c91aaafb981c52a56b9f0ab20d
Bambang Priyambodo <bambangpriyambodi21@gmail.com>
2024-05-01 10:31:26 +0700

    [FIRST COMMIT]

---
+++ test/Program.cs
@@ -0,0 +1,47 @@
+﻿// C# Program with Query Command
+using MySql.Data.MySqlClient;
+using System;
+internal class Program
+{
+    static void select()
+    {
+        string connectionString = "server=localhost;user=root;database=jobify;password=1m2h3b4y5W&";
+
+        MySqlConnection connection = new MySqlConnection(connectionString);
+        connection.Open();
+        String query = "select * from manu";
+        MySqlCommand command = new MySqlCommand(query, connection);
+        MySqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            String data = reader.GetString(1);
+            Console.WriteLine(data);
+        }
+
+        connection.Close();
+    }
+
+    static void insert()
+    {
+        string connectionString = "server=localhost;user=root;database=jobify;password=1m2h3b4y5W&";
+
+        MySqlConnection connection = new MySqlConnection(connectionString);
+        connection.Open();
+        string query = "insert into manu values ('ok','ok',2,2)";
+        MySqlCommand command = new MySqlCommand(query, connection);
+        int reader = command.ExecuteNonQuery();
+
+        if (reader > 0)
+        {
+            Console.WriteLine("data updated");
+        }
+
+        connection.Close();
+    }
+
+    private static void Main(string[] args)
+    {
+        select();
+    }
+}
